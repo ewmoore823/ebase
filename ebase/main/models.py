69923aa6f0b4627a6f3a630962dbbb36398997c1
Emmett Moore <ewmoore823@gmail.com>
2016-03-29 22:45:11 -0400

    first model and migration

--- ebase/main/models.py
+++ ebase/main/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Climb(models.Model):
+    name = models.CharField(max_length=20)
+    grade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
