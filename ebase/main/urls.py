ad15d85248dc543c6a6784bf99f3405151e5b069
Emmett Moore <ewmoore823@gmail.com>
2016-04-02 17:01:37 -0400

    first endpoint

--- ebase/main/urls.py
+++ ebase/main/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.main, name='main'),
+    url(r'^climbs/$', views.climbs, name='climbs'),
 ]
--- ebase/main/views.py
+++ ebase/main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+from main.models import Climb
 
+def main(request):
+    return HttpResponse("this is the main page :)")
+
+def climbs(request):
+    climbs = Climb.objects.all()
+
+    context = { 'climbs': climbs, }
+    return HttpResponse(render(request, 'main/climbs.html', context))
 
 # Create your views here.
