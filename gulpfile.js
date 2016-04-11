e5bd42d92cafceac3a285e7f06b9c7aaa01cf1a2
Emmett Moore <ewmoore823@gmail.com>
2016-04-11 00:02:00 -0400

    added sourcemaps for coffee compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
-
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['coffee']);
 
 gulp.task('coffee', function() {
     gulp.src('./ebase/main/static/main/coffee/*.coffee')
+        .pipe(sourcemaps.init())
         .pipe(coffee({bare:true}).on('error', gutil.log))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./ebase/main/static/main/lib/'));
 });
+
